c3c8309039966861d996cfb6501cbcf3fd2cd0b3
bervalim <bernardogvalim@gmail.com>
2024-02-14 19:17:39 -0300

    feat/pages-routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,13 +2,15 @@ import { Route, Routes } from "react-router-dom";
 import { Login } from "../pages/Login";
 import { Register } from "../pages/Register";
 import { Dashboard } from "../pages/Dashboard";
+import { ErrorPage } from "../pages/ErrorPage";
 
 export const RoutesMain = () => {
   return (
     <Routes>
-      <Route path="/" element={Login} />
-      <Route path="/register" element={Register} />
-      <Route path="/dashboard" element={Dashboard} />
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="*" element={<ErrorPage />} />
     </Routes>
   );
 };
