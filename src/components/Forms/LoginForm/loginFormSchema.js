0013169c8c0fc2ad88a2e25ed638082c18226a9b
bervalim <bernardogvalim@gmail.com>
2024-02-16 10:04:43 -0300

    fix/email-loginFormShema

--- src/components/Forms/LoginForm/loginFormSchema.js
+++ src/components/Forms/LoginForm/loginFormSchema.js
@@ -1,9 +1,6 @@
 import { z } from "zod";
 
 export const loginFormSchema = z.object({
-  email: z
-    .string()
-    .nonempty("O e-mail é obrigatório")
-    .email("Forneça um e-mail válido"),
+  email: z.string().nonempty("O e-mail é obrigatório"),
   password: z.string().nonempty("A senha é obrigatória"),
 });
