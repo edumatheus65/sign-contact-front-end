30a7b65887c96347a93447e9b9e8702c0dde0b0f
bervalim <bernardogvalim@gmail.com>
2024-02-15 22:24:28 -0300

    feat/handle-login-form

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -4,7 +4,6 @@ import { RegisterForm } from "../../components/Forms/RegisterForm";
 export const Register = () => {
   return (
     <DefaultTemplate>
-      <h2>Register Page</h2>
       <RegisterForm />
     </DefaultTemplate>
   );
