6c97320caee6071ba64002950a4ba5fd7c78b84e
bervalim <bernardogvalim@gmail.com>
2024-02-20 19:46:43 -0300

    feat/private-routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,13 +3,16 @@ import { Login } from "../pages/Login";
 import { Register } from "../pages/Register";
 import { Dashboard } from "../pages/Dashboard";
 import { ErrorPage } from "../pages/ErrorPage";
+import { PrivateRoutes } from "./PrivateRoutes";
 
 export const RoutesMain = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route element={<PrivateRoutes />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Route>
       <Route path="*" element={<ErrorPage />} />
     </Routes>
   );
