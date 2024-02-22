8314c9fa06a25a2fe1df8dcb37e4045aab99760d
bervalim <bernardogvalim@gmail.com>
2024-02-22 18:04:34 -0300

    feat/structure-create-modal

---
+++ src/components/Modals/CreateContactModal/index.jsx
@@ -0,0 +1,58 @@
+import { useForm } from "react-hook-form";
+import { IoCloseSharp } from "react-icons/io5";
+import { Input } from "../../Forms/Input";
+import { IoPersonAddSharp } from "react-icons/io5";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { CreateContactModalSchema } from "./CreateContactModalSchema";
+
+export const CreateContactModal = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(CreateContactModalSchema),
+  });
+
+  // const submitNewContact = (formData)=>{
+
+  // }
+  return (
+    <div role="dialog">
+      <div>
+        <div>
+          <h3>ADICIONAR CONTATO</h3>
+          <button title="fechar" aria-label="close">
+            <IoCloseSharp />
+          </button>
+        </div>
+        <form>
+          <Input
+            type="text"
+            placeholder="Nome Completo:"
+            {...register("fullName")}
+            error={errors.fullName}
+          />
+          <Input
+            type="email"
+            placeholder="E-mails:"
+            {...register("email")}
+            error={errors.email}
+          />
+          <Input
+            type="text"
+            placeholder="Telefones:"
+            {...register("phone")}
+            error={errors.phone}
+          />
+          <div>
+            <button type="submit">
+              Adicionar
+              <IoPersonAddSharp size={20} />
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
