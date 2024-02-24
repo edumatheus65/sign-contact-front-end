fc8ad70e0cd86b56784b1e547c07727ebe247524
bervalim <bernardogvalim@gmail.com>
2024-02-24 00:47:50 -0300

    feat/styling-delete-modal

--- src/components/Modals/DeleteContactModal/index.jsx
+++ src/components/Modals/DeleteContactModal/index.jsx
@@ -1,14 +1,15 @@
 import { useContext } from "react";
 import { ContactContext } from "../../../providers/ContactContext";
 import { IoCloseSharp } from "react-icons/io5";
+import styles from "./style.module.scss";
 
 export const DeleteContactModal = ({ contact, onClose }) => {
   const { deleteContact } = useContext(ContactContext);
 
   return (
-    <div role="dialog">
-      <div>
-        <div>
+    <div role="dialog" className={styles.modalOverlay}>
+      <div className={styles.modalBox}>
+        <div className={styles.ModalHeaders}>
           <h3 className="title two">CONFIRMAÇÃO DE EXCLUSÃO</h3>
           <button
             onClick={onClose}
@@ -19,21 +20,21 @@ export const DeleteContactModal = ({ contact, onClose }) => {
             <IoCloseSharp size={22} />
           </button>
         </div>
-        <div>
+        <div className={styles.Texts}>
           <h3 className="title three">
             Tem certeza que deseja excluir este contato?
           </h3>
-          <p className="paragraph">
+          <p className="title five">
             Essa ação não poderá ser desfeita, então pedimos que tenha cautela
-            antes de concluir
+            antes de concluir.
           </p>
         </div>
-        <div>
+        <div className={styles.Buttons}>
           <button className="signupBtn" onClick={onClose}>
             Cancelar
           </button>
           <button className="btn" onClick={() => deleteContact(contact.id)}>
-            Sim,excluir este contato
+            Excluir
           </button>
         </div>
       </div>
