e902f7e3054fabbb85286b1ec2c80beb4822c218
bervalim <bernardogvalim@gmail.com>
2024-02-24 01:11:07 -0300

    feat/styling-headerDashboard

--- src/components/HeaderDashboard/index.jsx
+++ src/components/HeaderDashboard/index.jsx
@@ -2,13 +2,14 @@ import { useContext } from "react";
 import Logo from "../../assets/Logo.png";
 import { RiLogoutCircleLine } from "react-icons/ri";
 import { ClientContext } from "../../providers/ClientContext";
+import styles from "./style.module.scss";
 
 export const HeaderDashboard = () => {
   const { clientLogout } = useContext(ClientContext);
   return (
     <header>
       <div className="container">
-        <div>
+        <div className={styles.flexbox}>
           <img src={Logo} alt="Logo Sign Contact" />
           <button className="signupBtn" onClick={() => clientLogout()}>
             Sair <RiLogoutCircleLine />
