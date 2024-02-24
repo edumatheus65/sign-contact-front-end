117086bc8294014a47b502080feee70e607df6ce
bervalim <bernardogvalim@gmail.com>
2024-02-24 03:17:29 -0300

    fix/styles

--- src/components/Cards/ContactCard/index.jsx
+++ src/components/Cards/ContactCard/index.jsx
@@ -1,12 +1,13 @@
 import { useContext } from "react";
 import { ContactContext } from "../../../providers/ContactContext";
 import { ContactList } from "./ContactList";
+import styles from "./style.module.scss";
 
 export const ContactCard = () => {
   const { contactList } = useContext(ContactContext);
 
   return (
-    <ul>
+    <ul className={styles.contactCard}>
       {contactList.length > 0
         ? contactList?.map((contact) => (
             <div key={contact.id}>
