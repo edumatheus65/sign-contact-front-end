59cf96453ef4eb5301ca277c1414ac80a245475b
bervalim <bernardogvalim@gmail.com>
2024-02-24 20:14:42 -0300

    feat/styling-ErrorPage

--- src/pages/ErrorPage/index.jsx
+++ src/pages/ErrorPage/index.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { ClientContext } from "../../providers/ClientContext";
 import { Link } from "react-router-dom";
+import styles from "./style.module.scss";
 
 export const ErrorPage = () => {
   const { client } = useContext(ClientContext);
@@ -9,20 +10,26 @@ export const ErrorPage = () => {
     <main>
       <div className="container">
         {client ? (
-          <>
-            <h2>Seja bem-vindo,{client.fullName}</h2>
-            <Link to="/dashboard">Cadastre o seu contato aqui:</Link>
-          </>
+          <div className={styles.flexbox}>
+            <h2 className="title yellow">Seja bem-vindo,{client.fullName}</h2>
+            <Link className="paragraph" to="/dashboard">
+              Cadastre o seu contato aqui:
+            </Link>
+          </div>
         ) : (
-          <div>
+          <div className={styles.flexbox}>
             <h2>Faça login agora</h2>
-            <Link to="/" state={{ lastRoute: "*" }}>
+            <Link className="paragraph" to="/" state={{ lastRoute: "*" }}>
               Login
             </Link>
           </div>
         )}
-        <h1>Erro :404</h1>
-        <p>Não foi possível encontrar a página</p>
+        <div className={styles.flexbox}>
+          <h1 className="title one">Erro :404</h1>
+          <p className="paragraph failed">
+            Não foi possível encontrar a página
+          </p>
+        </div>
       </div>
     </main>
   );
