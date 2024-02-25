82cc77f995cb84c739023ae4e03693abae8aefa8
bervalim <bernardogvalim@gmail.com>
2024-02-25 18:52:48 -0300

    feat/update-modal-schema

--- src/components/Modals/CreateContactModal/CreateContactModalSchema.js
+++ src/components/Modals/CreateContactModal/CreateContactModalSchema.js
@@ -3,5 +3,11 @@ import { z } from "zod";
 export const CreateContactModalSchema = z.object({
   fullName: z.string().min(3, "É necessário informar o nome completo!"),
   email: z.string().min(3, "O e-mail é obrigatório!"),
-  phone: z.string().min(3, "O telefone é obrigatório!"),
+  phone: z
+    .string()
+    .min(3, "O telefone é obrigatório!")
+    .regex(
+      /^\(?(?:\d{2})\)?[-. ]?(?:\d{4,5})[-. ]?\d{4}$/,
+      "Por favor, insira um número de telefone válido no formato (XX) XXXX-XXXX ou (XX) XXXXX-XXXX."
+    ),
 });
